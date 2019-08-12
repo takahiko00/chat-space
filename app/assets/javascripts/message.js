ec5a32d71e01f1d962cbe69e2dc249c310fa38dd
早川涼佑平 <hayakawaryouyuutaira@hayakawaryouyuutairanoMacBook-Pro.local>
2019-08-12 12:23:37 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <p class="upper-message__user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="upper-message__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message_body">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({  
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      var target = $('.message').last();
+      var position = target.offset().top + $('.messages').scrollTop();
+      $('.messages').animate({
+        scrollTop: position
+      }, 300, 'swing');  
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.submit-btn').prop('disabled', false);　//ここで解除している
+    })
+    
+  })
+});
+
