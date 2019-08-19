857d10dedcc693968a250b3ea3a8046a4dde486c
早川涼佑平 <hayakawaryouyuutaira@hayakawaryouyuutairanoMacBook-Pro.local>
2019-08-19 12:13:09 +0900

    komanndo

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.0'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:chat-space/takahiko00.git'
+set :repo_url,  'git@github.com:takahiko00/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
